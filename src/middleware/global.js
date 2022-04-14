4e8ed65a2c1f22390b55bb1f9084ce4921ae35b8
mistermiracl <mm@gmail.com>
2022-04-14 17:46:26 -0500

    minor: heroicons svgs global view helper

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -4,6 +4,40 @@
  * @typedef {import('express').Response} Response
  */
 
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * Returns a heroicons svg
+ * @param {string} name heroicon name, prefixed with "hero-"
+ */
+function svg(name, className) {
+    // TODO: optimized, all these conditions can be simplified by checking the split for each, reducing it to just one lookup
+    if (name.startsWith('hero-') && (name.includes('-s-') || name.includes('-o-'))) {
+        // NOTE: make the readfile async?
+        // NOTE: add all the svg in the public directory?
+        const nameParts = name.split('-');
+        const heroiconType = nameParts[1];
+        const heroiconName = nameParts[2] + '.svg';
+        
+        var heroiconsTypeFolder = '';
+        if(heroiconType == 's') {
+            heroiconsTypeFolder = 'solid';
+        } else if(heroiconType == 'o') {
+            heroiconsTypeFolder = 'outline';
+        }
+
+        try {
+            const svg = fs.readFileSync(path.join(projectRoot, 'node_modules', 'heroicons', heroiconsTypeFolder, heroiconName)).toString();
+            const svgParts = svg.split('<svg ');
+            return `<svg ${className ? `class="${className}"` : ''} ${svgParts[1]}`;
+        } catch(e) {
+            throw e;
+        }
+    }
+    return '';
+}
+
 /**
  * 
  * @param {Request} req 
@@ -12,5 +46,6 @@
  */
 module.exports = (req, res, next) => {
     res.locals.currentUrl = req.url;
+    res.locals.svg = svg
     next();
 };
