93640e40146eff57f9ff748780b1f4e544035f39
mistermiracl <mm@gmail.com>
2022-04-22 17:07:39 -0500

    feat: configured session mechanism

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
-const router = require('./routes/routes');
+
 const middleware = require('./middleware');
+const router = require('./routes/routes');
+
+// globals
+// TODO: should it be like this?, using the global object?
+global.projectRoot = path.dirname(__dirname);
+global.isDev = process.env.NODE_ENV === 'development';
+global.isProd = process.env.NODE_ENV === 'production';
 
 const app = express();
 
+// static
 app.use('/', express.static(path.join(path.dirname(__dirname), 'public')));
 // views path
 app.set('views', path.join(__dirname, 'views'));
@@ -16,9 +28,38 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', './layouts/index');
 
+// session
+/**
+ * @type {session.SessionOptions}
+ */
+const sessionConfig = {
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.APP_SECRET
+};
+if(isDev) {
+    // TODO: test on linux
+    sessionConfig.store = new FileStore({
+        path: path.join(projectRoot, '.sessions')//^1.5.0
+    });
+} else {
+    // TODO: use gap memcache
+    sessionConfig.store = null;
+}
+app.use(session(sessionConfig));
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(middleware);
+
+// router
 app.use('/', router(app));
 
 app.listen(process.env.PORT, () => {
     console.log('Server started at: %s', process.env.PORT);
 });
+
