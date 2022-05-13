a7c458cee430f569e9804a4b2703999496d88767
mistermiracl <mm@gmail.com>
2022-05-13 00:13:36 -0500

    feat: on admin panel, added 'special' seasons support, added chapters management, added cover and source file upload support, using admin middleware to prevent unauthenticated requests

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -6,7 +6,8 @@
 
 const status = require('http2').constants;
 
-const { seasonModel } = require('../models');
+const { seasonModel, chapterModel, rotatingImagePairModel } = require('../models');
+const cloudStorage = require('../common/cloud-storage');
 
 /**
  * 
@@ -14,9 +15,6 @@ const { seasonModel } = require('../models');
  * @param {Response} res 
  */
 function index(req, res) {
-    if(!req.session.authenticated) {
-        return res.sendStatus(status.HTTP_STATUS_UNAUTHORIZED);
-    }
     res.render('admin/index');
 }
 
@@ -83,6 +81,7 @@ async function season(req, res) {
         case 'POST':
             // remove empty values
             Object.keys(req.body).forEach((k) => !req.body[k] && delete req.body[k]);
+            req.body.special = req.body.special === 'on' ? true : false;
             if(id) {
                 await seasonModel.update(id, req.body);
             } else {
@@ -112,7 +111,7 @@ async function deleteSeason(req, res) {
  * @param {Response} res 
  */
 function images(req, res) {
-
+    res.send('ok');
 }
 
 /**
@@ -133,12 +132,153 @@ function deleteImage(req, res) {
 
 }
 
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+async function chapters(req, res) {
+    var seasonId = req.query.seasonId;
+    if(!seasonId) {
+        seasonId = (await seasonModel.findByNumber(1)).id;
+    }
+    var chapters;
+    chapters = await chapterModel.allBySeason(seasonId);
+    chapters.forEach(c => {
+        c.posterUrl = cloudStorage.publicUrl(c.poster);
+        c.sourceUrl = cloudStorage.publicUrl(c.source);
+    });
+    
+    res.locals.selectedSeasonId = seasonId;
+    res.locals.chapters = chapters;
+    res.locals.seasons = await seasonModel.all();
+    
+    res.render('admin/chapters/index');
+}
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+async function chapter(req, res) {
+    const id = req.params.id;
+    const seasonId = req.query.seasonId;
+    
+    var chapter;
+    res.locals.errors = {};
+    switch(req.method) {
+        case 'GET':
+            chapter = {};
+            if(id) {
+                chapter = await chapterModel.find(id, seasonId);
+                chapter.posterUrl = cloudStorage.publicUrl(chapter.poster);
+                chapter.sourceUrl = cloudStorage.publicUrl(chapter.source);
+            }
+            res.locals.chapter = chapter;
+            res.locals.seasonId = seasonId;
+            res.render('admin/chapters/chapter');
+            break;
+        case 'POST':
+            const poster = req.files['poster'];
+            const objectPrefix = seasonId + '/';
+            if(id) {
+                console.log('Retrieving chapter');
+                let chapter = await chapterModel.find(id, seasonId);
+                if(poster) {
+                    console.log('There is a poster: ' + poster);
+                    const prevPosterName = chapter.poster;
+                    if(prevPosterName) {
+                        console.log('Removing previous poster');
+                        await cloudStorage.remove(prevPosterName);
+                    }
+                    const posterName = await cloudStorage.upload(objectPrefix, poster);
+                    chapter.poster = posterName;
+                }
+                if(req.body.source !== chapter.source) {
+                    console.log('There is a new source: ' + chapter.source);
+                    console.log('Removing previous source');
+                    await cloudStorage.remove(chapter.source);
+                }
+                chapter = { ...chapter, ...req.body };
+                console.log('Updating chapter');
+                await chapterModel.update(chapter);
+            } else {
+                const chapter = req.body;
+                const posterName = await cloudStorage.upload(objectPrefix, poster);
+                chapter.poster = posterName;
+                await chapterModel.insert(chapter, seasonId);
+                // console.log(res[0].mutationResults[0].key);
+            }
+
+            res.redirect('/admin/chapters/?seasonId=' + seasonId);
+            break;
+        default:
+            res.sendStatus(status.HTTP_STATUS_NOT_FOUND);
+            break;
+    }
+}
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+async function chapterUpload(req, res) {
+    const seasonId = req.body.seasonId;
+    const filename = req.body.filename;
+    const mimeType = req.body.mimeType;
+    const objectPrefix = seasonId + '/';
+    const [ key, signedUrl ] = await cloudStorage.generateSignedWriteUrl(objectPrefix, filename, mimeType);
+    res.send({
+        key,
+        signedUrl
+    });
+}
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+async function deleteChapter(req, res) {
+    const id = req.params.id;
+    const seasonId = req.query.seasonId;
+    const chapter = await chapterModel.find(id, seasonId);
+    await cloudStorage.remove(chapter.poster);
+    await cloudStorage.remove(chapter.source);
+    await chapterModel.remove(id, seasonId);
+    res.redirect('/admin/chapters/?seasonId=' + seasonId);
+}
+
+
+/**
+ * 
+ * @param {Request} req 
+ * @param {Response} res 
+ */
+async function deleteChapterSource(req, res) {
+    const id = req.params.id;
+    const seasonId = req.query.seasonId;
+    const chapter = await chapterModel.find(id, seasonId);
+    await cloudStorage.remove(chapter.source);
+    // TODO: remove source from database
+    // chapter.source = null;
+    // await chapterModel.update(chapter);
+    res.send('ok');
+}
+
 module.exports = {
     index,
     login,
     seasons,
     season,
     deleteSeason,
+    chapters,
+    chapter,
+    chapterUpload,
+    deleteChapter,
+    deleteChapterSource,
     images,
     image,
     deleteImage
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
+const router = require('express').Router();
+
+const { adminMiddleware, multerMiddleware } = require('../middleware');
+
 const IndexController = require('../controllers/index-controller');
 const SeasonController = require('../controllers/seasons-controller');
 const AdminController = require('../controllers/admin-controller');
-
-const router = require('express').Router();
 
 /**
  * 
@@ -10,21 +12,32 @@ const router = require('express').Router();
  */
 module.exports = app => {
     
-    router.get('/admin', AdminController.index);
+    // index
+    router.get('/', IndexController.index);
+
+    // seasons
+    router.get('/seasons/:slug?', SeasonController.index);
+    router.get('/seasons/:seasonSlug/:chapterSlug', SeasonController.chapter);
+
+    // admin
     router.get('/admin/login', AdminController.login);
     router.post('/admin/login', AdminController.login);
+    router.use(adminMiddleware);
+    router.get('/admin', AdminController.index);
     router.get('/admin/seasons', AdminController.seasons);
     router.get('/admin/season/:id?', AdminController.season);
     router.post('/admin/season/:id?', AdminController.season);
     router.get('/admin/season/:id/delete', AdminController.deleteSeason);
+    router.get('/admin/chapters', AdminController.chapters);
+    router.get('/admin/chapter/:id?', AdminController.chapter);
+    router.post('/admin/chapter/upload-url', AdminController.chapterUpload);
+    // NOTE: just like in fast api param urls must be declared later
+    router.post('/admin/chapter/:id?', multerMiddleware, AdminController.chapter);
+    router.get('/admin/chapter/:id/delete', AdminController.deleteChapter);
+    router.delete('/admin/chapter/:id/source', AdminController.deleteChapterSource);
     router.get('/admin/images', AdminController.images);
     router.get('/admin/image/:id?', AdminController.image);
     router.post('/admin/image/:id?', AdminController.image);
-
-    router.get('/', IndexController.index);
-
-    router.get('/seasons/:slug?', SeasonController.index);
-    router.get('/seasons/:seasonSlug/:chapterSlug', SeasonController.chapter);
 
     return router;
 };
